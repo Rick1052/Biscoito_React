d2b40e39cf3f82a1c4d8914f59ffb53e8b1bda85
Rick1052 <sant.gp.henrique@gmail.com>
2024-05-14 22:11:21 -0300

    aula14-05

--- src/App.js
+++ src/App.js
@@ -20,4 +20,33 @@ class App extends Component{
       'A maior barreira para o sucesso é o medo do fracasso'
     ];
   }
+
+  quebrarBiscoito(){
+    let state = this.state;
+    let numeroAleatorio = Math.floor(Math.random() * this.frases.length);
+    state.textoFrase = this.frases[numeroAleatorio];
+    this.setState(state);
+  }
+
+  render(){
+    return(
+      <div className="container"> 
+        <img src={Biscoito} className="img" />
+        <Botao nome="Abrir biscoito" acao={this.quebrarBiscoito}/>
+        <h3 className="textoFrase">{this.state.textoFrase}</h3>
+      </div>
+    );
+  }
 }
+
+class Botao extends Component{
+  render(){
+    return(
+      <div>
+        <button onClick={this.props.acao}>{this.props.nome}</button>
+      </div>
+    );
+  }
+}
+
+export default App;
